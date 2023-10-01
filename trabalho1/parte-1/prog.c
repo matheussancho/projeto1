a7f574699f7a3478e68d3ca34db43ab8675e866e
matheus <matheussancho13@gmail.com>
2023-10-01 19:03:59 -0300

    adjust readme.dm

--- trabalho1/parte-1/prog.c
+++ trabalho1/parte-1/prog.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-// Função para somar os elementos de um array de doubles
+// Função para somar os itens de um array de doubles
 double somaArray(double array[], int n) {
     double soma = 0.0;
     for (int i = 0; i < n; i++) {
@@ -9,7 +9,7 @@ double somaArray(double array[], int n) {
     return soma;
 }
 
-// Função para calcular a média dos elementos de um array de doubles
+// Função para calcular a média dos itens de um array com elementos double float
 double mediaArray(double array[], int n) {
     if (n == 0) {
         return 0.0; // Evitando divisão por zero
@@ -19,7 +19,7 @@ double mediaArray(double array[], int n) {
     return soma / n;
 }
 
-// Função para encontrar o maior elemento em um array de doubles
+// Função para encontrar o maior item em um array com elementos double float
 double maiorElementoArray(double array[], int n) {
     if (n == 0) {
         return 0.0; // Array vazio
@@ -37,20 +37,21 @@ double maiorElementoArray(double array[], int n) {
 int main() {
     int n;
 
-    // Solicitar o número de elementos ao usuário
+    // Aqui o usuário vai fornecer o número de elementos do array com elementos double float
     printf("Digite o número de elementos no array: ");
     scanf("%d", &n);
 
-    // Criar um array de doubles com o tamanho especificado
+    // Cria um array com elementos double float do tamanho especificado
     double array[n];
 
-    // Preencher o array com valores fornecidos pelo usuário
+    // Preencher o array com valores fornecidos pelo usuário 
+    // Não temos condição para erro de digitação
     printf("Digite os elementos do array:\n");
     for (int i = 0; i < n; i++) {
         scanf("%lf", &array[i]);
     }
 
-    // Chamar e imprimir os resultados das três funções
+    // Request para as 3 funções e print do resultado
     printf("Soma dos elementos: %.2lf\n", somaArray(array, n));
     printf("Média dos elementos: %.2lf\n", mediaArray(array, n));
     printf("Maior elemento: %.2lf\n", maiorElementoArray(array, n));
